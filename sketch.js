27cb00b4aeb48ba2fa46bb43d309ffc2b458a13f
rkriva11 <rkriva1@lsu.edu>
2025-09-10 13:52:10 -0500

    trying my best

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
 function setup() {
-  createCanvas(400, 269);
+  createCanvas(950, 642);
+  background(214, 214, 205);
+  noLoop();
 }
 
 function draw() {
+  
+  fill(0)
+  strokeWeight(20);
+  line (0, 169, 437, 550);
+  
+  
+  //console. log(mouseX, mouseY);
 
+  
+
+  fill(255);
+  textSize(16);
+  text(mouseX +"," + mouseY, mouseX, mouseY);
 }
+
+
+
+    
+  
+
+
+
+
+
+
+
+
