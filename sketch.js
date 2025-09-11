dfc911cf5baf791b2611ab1611ec063d994f9483
rkriva11 <rkriva1@lsu.edu>
2025-09-10 23:07:38 -0500

    DONE :3

--- sketch.js
+++ sketch.js
@@ -6,18 +6,123 @@ function setup() {
 
 function draw() {
   
-  fill(0)
-  strokeWeight(20);
-  line (0, 169, 437, 550);
-  
-  
-  //console. log(mouseX, mouseY);
+  // ==== COLORS====
 
+  
+  stroke(2, 10, 48);
+  strokeWeight(60);
+  line(4, 340, 253, 640);
+ line(0, 300,365, 640 );
+  line(188, 545,300, 640 );
+  line(0, 400 ,150, 640);
+  line(292, 545,395, 640 );
+  line(125, 505,214, 640 );
+  line(60, 506,84, 640 );
+
+  strokeWeight(60);
+  line(517, 360,700, 640 );
+  line(545, 335, 860, 640 );
+  line(560, 403,780, 640 );
+  line(475, 365, 645, 640);
+  line(642, 640, 855, 640);
+
+  stroke(115, 21, 13);
+  strokeWeight(60);
+  line(263, 328, 404, 433);
+  line(305, 345,400, 412 );
+
+strokeWeight(70);
+  line(375, 360, 395, 412 );
+
+  strokeWeight(60);
+  line(418, 463, 488, 640 );
+  line(426, 486, 573, 640 );
+  line(475, 640, 566, 640 );
+  line(470, 509, 540, 590);
+  line(500, 515, 583, 640 );
+  
+  strokeWeight(20);
+  line(460, 477,480, 493 );
+
+  strokeWeight(20);
+  stroke(214, 214, 205);
+  line(365, 320,430, 330 ); 
+
+  stroke(220, 186, 63);
+  strokeWeight(50);
+  line(8, 480, 8, 640);
+  
+  strokeWeight(30);
+  line(3, 630, 65, 630);
+
+  stroke(20, 20, 19);
+  strokeWeight(20);
+  line(925, 323, 950, 323 );
+
+
+
+
+// ====LINE ART====
+ 
+  fill(0)
+  stroke(15, 15, 10);
+  strokeWeight(20);
+  line (0, 261, 430, 640);
+  line (180, 414, 260, 290 );
+  line (0, 176, 390, 345);
+  line (390,345, 445, 330);
+  line (445, 330, 482,269 );
+  line (440, 342, 507, 345);
+  line (507, 345, 550, 295);
+  line (550, 295, 570, 265);
+  line (570, 265, 580, 208 );
+  line (580, 208, 577, 123);
+  line (495, 93, 539, 111 );
+  line (539, 111,584, 108);
+  line (584, 108, 638, 86);
+  line (638, 86, 652, 65);
+  line (652, 65, 655, 5 );
+  
+  strokeWeight(10);
+  line (655, 5, 578, 1);
+
+  strokeWeight(20);
+  line (238, 341, 390, 450);
+  line (390, 450, 453, 641);
+  line( 440, 448, 418, 350);
+  line(440, 448, 536, 520 );
+  line (400, 470, 500, 600);
+  line(450, 356, 610, 640 );
+  line(390, 460, 437, 432);
+  line(557, 305, 910, 642 );
+
+  strokeWeight(10);
+  line(421, 62, 490, 91 );
+  line(381, 26, 478, 74 );
+  line(377, 14, 453, 52 );
+  line(0, 160, 1, 42 );
+
+  strokeWeight(20);
+  line(0, 415, 16, 430);
+  line(0, 450, 45, 495);
+  line(0, 460, 57, 558);
+  line(35, 550, 45, 642 );
+  line(40, 618, 34, 641 );
+  line(53, 615, 103,642 );
+
+  strokeWeight(10);
+  line(713, 108, 950, 108);
+
+
+  
   
 
   fill(255);
   textSize(16);
   text(mouseX +"," + mouseY, mouseX, mouseY);
+
+
+  //console. log(mouseX, mouseY);
 }
 
 
